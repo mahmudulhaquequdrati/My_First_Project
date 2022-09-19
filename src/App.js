36a43206e069545c3b14e69899b337e21459393a
mahmudulhaquequdrati <freelancermahmudqudrati@gmail.com>
2022-09-19 19:25:09 +0600

    static frontend done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Page404 from "./components/NotFound";
+import SignIn from "./components/SignIn";
+import SignUp from "./components/SignUp";
+import Home from "./Pages/Home/Home";
+import ProductDetails from "./Pages/ProductDetails/ProductDetails";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/join-us" element={<SignUp />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/product/:productId" element={<ProductDetails />} />
+        <Route path="*" element={<Page404 />} />
+      </Routes>
+    </>
   );
 }
 
