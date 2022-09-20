72505d66a62cada9ae5db88835183a855993de4b
mahmudulhaquequdrati <freelancermahmudqudrati@gmail.com>
2022-09-20 22:02:53 +0600

    project ui and backend implementation done

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import Page404 from "./components/NotFound";
 import SignIn from "./components/SignIn";
 import SignUp from "./components/SignUp";
+import AddProduct from "./Pages/AddProduct/AddProduct";
 import Home from "./Pages/Home/Home";
 import ProductDetails from "./Pages/ProductDetails/ProductDetails";
 
@@ -14,6 +15,8 @@ function App() {
         <Route path="/join-us" element={<SignUp />} />
         <Route path="/sign-in" element={<SignIn />} />
         <Route path="/product/:productId" element={<ProductDetails />} />
+
+        <Route path="/profile" element={<AddProduct />} />
         <Route path="*" element={<Page404 />} />
       </Routes>
     </>
